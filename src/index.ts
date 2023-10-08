cf4512c7baf2d9805d150ec82f6a7cf09848c787
Seong-Hoon-Lim <hooney200@gmail.com>
2023-10-08 23:15:45 +0900

    #4 - DB 접근 로직 구현
     express 앱과 postgreSQL DB 연결 정의
    서버 동작 확인 시 에러 발생됨 (postgreSQL 서버에 연결 되지 않았기 때문)
    doker 를 활용하여 postgreSQL 서버에 연결 필요

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
+import {AppDataSource} from "./data-source";
+import {User} from "./entity/User";
+import {DataSource} from "typeorm";
+
+const PORT: number = 4000;
 
 const app: Express = express();
-const PORT: Number = 4000;
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log('Data Source has been initialized')
+    })
+    .catch((err) => {
+        console.error('Error during Data Source initialization', err);
+    })
 
 app.listen(PORT, (): void => {
     console.log(`Server Running at http://localhost:${PORT}`);
