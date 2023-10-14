3e97d474227ff82da4c6c5f1b61d9b5c2cb2376d
Seong-Hoon-Lim <hooney200@gmail.com>
2023-10-14 23:57:35 +0900

    #10 - jwt access_token 발급 로직 구현
    `postman` 을 활용하여 액세스 토큰 발급 여부 정상 확인
    이후 refresh_token 역시 동일한 방법으로 발급 여부를 먼저 확인 하고 이후 인증 미들웨어 추가 예정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
+import jwt from 'jsonwebtoken';
 import {AppDataSource} from "./data-source";
 import {User} from "./entity/User";
 import {DataSource} from "typeorm";
@@ -18,7 +19,38 @@ AppDataSource
     })
     .catch((err) => {
         console.error('Error during Data Source initialization', err);
-    })
+    });
+
+//회원정보 DB 저장 로직
+app.post('/signup', async (req, res) => {
+    const { email, password, name } = req.body;
+    try {
+        //새로운 user 객체 생성
+        const user = new User();
+        user.email = email;
+        user.password = password;
+        user.name = name;
+        //DB 저장
+        await AppDataSource.getRepository(User).save(user);
+        res.status(201).send({ message: 'User registered successfully!', user: { id: user.id, email: user.email, name: user.name } });
+    } catch (error) {
+        console.error('Error during user registration:', error);
+        res.status(500).send({ message: 'Internal server error' });
+    }
+});
+
+app.post('/auth/token/access', async (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKKEN_SECRET);
+    res.json({ accessToken: accessToken });
+    console.log('accessToken: ' + accessToken);
+});
+
+let refreshTokens = [];
+app.post('/auth/login', (req, res) => {
+    
+});
 
 app.listen(process.env.PORT, (): void => {
     console.log(`Server Running at http://localhost:${process.env.PORT}`);
