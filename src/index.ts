387bd925a1602916e23f2b55c304be75c6b65015
Seong-Hoon-Lim <hooney200@gmail.com>
2023-10-16 22:57:35 +0900

    #12 - 로그인 및 회원 가입 기능 구현
    * JWT인증 과정에서 모두 구현 완료 /auth/login 경로에서 jwt 발급 및 인증 로직 하나로 합치도록 수정
    * 뷰를 보여주기 위한 핸들러 로직 추가
    * JWT 발급 및 검증 그리고 인증 로직 실제 뷰를 활용하여 테스트 시 문제 발생으로 해결 위해 일부 수정

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,13 @@ type JWTUser = {
 };
 import {DataSource} from "typeorm";
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 dotenv.config();
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
@@ -32,8 +34,16 @@ AppDataSource
         console.error('데이터 초기화 중 에러 발생: ', err);
     });
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//회원 가입 form 뷰
+app.get('/signup', (req: Request, res: Response) => {
+   res.render('signup');
+});
+
 //회원정보 DB 저장 로직
-app.post('/signup', async (req: Request, res: Response) => {
+app.post('/auth/signup', async (req: Request, res: Response) => {
     const { email, password, name } = req.body;
     try {
         //새로운 user 객체 생성
@@ -52,29 +62,47 @@ app.post('/signup', async (req: Request, res: Response) => {
     }
 });
 
-//로그인 시 jwt 토큰 발행 로직
+//로그인 화면 form 뷰
+app.get('/signin', (req: Request, res: Response) => {
+   res.render('signin');
+});
+
+//로그인 절차 로직(JWT 발급 및 유효성 검증과 인증 성공 시 로그인 성공)
 let refreshTokens: string[] = []; //나중에 refreshToken 은 DB 에 저장해줘야 됨
-app.post('/auth/login', async (req: Request, res: Response) => {
-    const email: string = req.body.email;
-    const user: JWTUser = { email: email };
+app.post('/auth/signin', async (req: Request, res: Response) => {
+    const { email } = req.body;
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const userEntity = await userRepository.findOne({ where: { email: email } });
 
-    //jwt 를 활용하여 액세스 토큰 발급 (유효기간 30초 설정)
-    const accessToken: string = jwt.sign(
-        user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
-    //jwt 를 활용하여 리프레시 토큰 발급 (유효기간 1일 설정)
-    const refreshToken: string = jwt.sign(
-        user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+        if (!userEntity) {
+            return res.status(404).json({ message: '회원을 찾을 수 없거나 비밀번호가 잘못됨' });
+        }
 
-    refreshTokens.push(refreshToken);
+        const user: JWTUser = { email: email };
 
-    //발급된 refreshToken 을 쿠키에 넣어주기
-    res.cookie('jwt', refreshToken, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000,
-    });
+        // JWT 토큰 발행
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
 
-    res.json({ accessToken: accessToken });
-    console.log('발급 된 accessToken: ' + accessToken);
+        refreshTokens.push(refreshToken);
+
+        // 토큰 쿠키에 저장
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+        });
+
+        // 인증된 사용자 정보 반환
+        res.json({ accessToken, userEntity });
+
+        console.log('로그인 및 토큰 발행 성공');
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error('로그인 중 에러 발생:', error);
+        }
+        res.status(500).send({ message: '내부 서버 에러' });
+    }
 });
 
 //발급된 리프레시 refreshToken 을 활용하여 accessToken 재발급 로직
@@ -85,7 +113,7 @@ app.get('/auth/refresh', (req: Request, res: Response) => {
 
     const refreshToken = cookies.jwt;
     //refreshToken 이 refreshTokens 의 [] 에 포함여부 검증
-    if (!refreshToken.includes(refreshToken)) {
+    if (!refreshTokens.includes(refreshToken)) {
         return res.sendStatus(403);
     }
 
@@ -103,14 +131,19 @@ app.get('/auth/refresh', (req: Request, res: Response) => {
     });
 });
 
-// 인증 미들웨어 로직
+//로그인 화면 form 뷰
+app.get('/signin', (req: Request, res: Response) => {
+    res.render('signin');
+});
+
+// 인증 미들웨어 로직 (로그인 된 회원이 접근 할 수 있는 엔드포인트에 적용)
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const authHeader: string | undefined = req.headers['authorization'];
-    const token: string | undefined= authHeader && authHeader.split(' ')[1];
+    const token: string | undefined = req.cookies.jwt;  // 쿠키에서 토큰을 가져옴
+
     if (token == null) return res.status(401).json({ message: '토큰이 제공되지 않았습니다.' });
 
     //토큰 유효성 검증
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET ?? '', (err: JsonWebTokenError | null, user: JWTUser) => {
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET ?? '', (err: JsonWebTokenError | null, user: JWTUser) => {
         if (err) {
             console.error(err);
             return res.sendStatus(403);
@@ -120,30 +153,9 @@ function authMiddleware(req: Request, res: Response, next: NextFunction) {
     });
 }
 
-//jwt 인증 로직(인증 성공 시 로그인 성공)
-app.post('/auth/signin', authMiddleware, async (req: Request, res: Response) => {
-    const email = req.user?.email;
-    if (!email) {
-        return res.status(400).json({ message: '회원 메일을 찾을 수 없음' });
-    }
-    try {
-        const userRespository = AppDataSource.getRepository(User);
-        const userEntity = await userRespository.findOne({ where: { email: email } });
-        if (!userEntity) {
-            return res.status(404).json({ message: '회원을 찾을 수 없음' });
-        }
-        // 로그인 성공 시, 리프레시 토큰도 생성하고 쿠키로 클라이언트에게 전달
-        const user = { email: email };  // 이 부분을 추가
-        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-        res.cookie('jwt', refreshToken);
-        res.json(userEntity);
-        console.log('로그인 성공');
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            console.error('회원 찾는 중 에러 발생:', error);
-        }
-        res.status(500).send({ message: '내부 서버 에러' });
-    }
+//인증된 회원만 볼 수 있는 뷰
+app.get('/', authMiddleware, async (req: Request, res: Response) => {
+   res.render('index');
 });
 
 app.listen(process.env.PORT, (): void => {
